5bc817a968d91c5bfab31ee242de44ce76c7c4e0
cooler-SAI <cooler19860212@yahoo.com>
2025-01-10 21:10:06 +0300

    added config, misc updates

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"Weather-API/internal/api"
+	"Weather-API/internal/config"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	cnf := config.Load()
+
+	http.HandleFunc("/", api.WeatherHandler)
+
+	log.Printf("Starting server on port %s...", cfg.Port)
+	if err := http.ListenAndServe(":"+cfg.Port, nil); err != nil {
+		log.Fatalf("Error to start Server: %v", err)
+	}
+
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,27 @@
 package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"os"
+)
+
+type Config struct {
+	Port string
+}
+
+func Load() Config {
+	if err := godotenv.Load(); err != nil {
+		fmt.Println(".env file not found, using local .env")
+	}
+	return Config{
+		Port: getEnv("PORT", "8080"),
+	}
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
+}
