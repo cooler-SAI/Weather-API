b162af6e9979703e98d4be337792ba3046a1b246
cooler-SAI <cooler19860212@yahoo.com>
2025-01-15 21:20:30 +0300

    added link to get data

--- internal/service/weather.go
+++ internal/service/weather.go
@@ -1,15 +1,49 @@
 package service
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 type WeatherResponse struct {
 	City  string  `json:"city"`
 	Temp  float64 `json:"temperature"`
 	Units string  `json:"units"`
 }
 
-func GetWeatherData() WeatherResponse {
-	return WeatherResponse{
-		City:  "Moscow",
-		Temp:  -5.0,
-		Units: "Celsius",
+type openWeatherMapResponse struct {
+	Name string `json:"name"`
+	Main struct {
+		Temp float64 `json:"temp"`
+	} `json:"main"`
+}
+
+const (
+	apiKey = "2bfcf1d1a1c828a2562683b20b52eaa7"
+)
+
+func GetWeatherData(city string) (WeatherResponse, error) {
+	url := fmt.Sprintf("http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric&appid=%s",
+		city, apiKey)
+	resp, err := http.Get(url)
+	if err != nil {
+		return WeatherResponse{}, fmt.Errorf("failed to fetch weather data: %w", err)
 	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return WeatherResponse{}, fmt.Errorf("error: status code %d", resp.StatusCode)
+	}
+
+	var owmResp openWeatherMapResponse
+	if err := json.NewDecoder(resp.Body).Decode(&owmResp); err != nil {
+		return WeatherResponse{}, fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	return WeatherResponse{
+		City:  owmResp.Name,
+		Temp:  owmResp.Main.Temp,
+		Units: "Celsius",
+	}, nil
 }
