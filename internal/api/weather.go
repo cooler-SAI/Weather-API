b7ae64826c04370e53c86cc08c9e336ee1df4671
cooler-SAI <cooler19860212@yahoo.com>
2025-01-15 22:04:37 +0300

    update libs, set up redis use

--- internal/api/weather.go
+++ internal/api/weather.go
@@ -3,15 +3,30 @@ package api
 import (
 	"encoding/json"
 	"net/http"
+	"time"
+	"weather-api/internal/cache"
 	"weather-api/internal/service"
 
 	"github.com/rs/zerolog/log"
 )
 
+var redisClient = cache.NewRedisClient("localhost:6379")
+
 func WeatherHandler(w http.ResponseWriter, r *http.Request) {
 	city := r.URL.Query().Get("city")
 	if city == "" {
 		city = "Moscow"
+	}
+
+	cachedData, err := redisClient.Get(r.Context(), city).Result()
+	if err == nil {
+
+		w.Header().Set("Content-Type", "application/json")
+		_, err := w.Write([]byte(cachedData))
+		if err != nil {
+			return
+		}
+		return
 	}
 
 	weatherData, err := service.GetWeatherData(city)
@@ -21,9 +36,12 @@ func WeatherHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	weatherJSON, _ := json.Marshal(weatherData)
+	redisClient.Set(r.Context(), city, weatherJSON, time.Hour)
+
 	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(weatherData); err != nil {
-		log.Error().Err(err).Msg("Failed to encode response")
-		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+	_, err2 := w.Write(weatherJSON)
+	if err2 != nil {
+		return
 	}
 }
